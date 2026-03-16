d429b10b378b29315fdb735b1ad4aaffec9119d9
Nycx04 <johndavecordoto04@gmail.com>
2026-03-16 22:35:20 +0800

    Add files via upload

---
+++ onmouseover.js
@@ -0,0 +1,6 @@
+document.getElementById("Img").onmouseover = function (){
+    document.getElementById("Img").src="11-1.thumb128.png";
+}; 
+document.getElementById("Img").onmouseout = function (){
+    document.getElementById("Img").src="10-1.thumb128.png";
+}; 
