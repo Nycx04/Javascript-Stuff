1f22a2f322e41d0c90298a8158b53a728f2be71a
john dave cordoto <johndavecordoto04@gmail.com>
2026-04-30 23:27:35 +0800

    nyanya

--- TempConverter.js
+++ TempConverter.js
@@ -4,6 +4,10 @@ function Solve() {
     let unit = document.querySelector('input[name="unit"]:checked').value;
 
     let result;
+    
+    if (value == "") {
+        alert("no input detected");
+    }
 
     if (unit === "c") {
         result = (value * (9 / 5)) + 32;
@@ -12,5 +16,5 @@ function Solve() {
     else {
         result = (value - 32) * (5 / 9);
         document.getElementById("output").value = result.toFixed(2) + " °C";
-    }
+    } 
 }
---
+++ Usernamevalid.js
@@ -0,0 +1,11 @@
+
+function check() {
+    let valueInput = document.getElementById("name").value;
+    if (valueInput.length > 5) {
+        alert("skibifdi");
+    }
+
+    if (valueInput.includes(" ")){
+        alert("helo");
+    }
+}  
